9e1314d89d586febd111c06b467715815268c149
sayus2884 <joniah2884@gmail.com>
2022-03-04 14:48:51 -0800

    move some styles to global css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,39 +4,46 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js</a> +{" "}
-        <a href="https://tailwindcss.com/">TailwindCSS!</a>
+      <h1 className="title">
+        Welcome to{" "}
+        <a className="text-blue-500" href="https://nextjs.org">
+          Next.js
+        </a>{" "}
+        +{" "}
+        <a className="text-blue-500" href="https://tailwindcss.com/">
+          TailwindCSS!
+        </a>
       </h1>
 
-      <p className={styles.description}>
-        Get started by editing <code className={styles.code}>pages/index.tsx</code>
+      <p>
+        Get started by editing <code>src/pages/index.tsx</code> or{" "}
+        <code>src/styles/globals.css</code>
       </p>
 
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
+      <div className="grid md:grid-cols-2 w-full items-ceter justify-center max-w-[800px]">
+        <a href="https://nextjs.org/docs" className="card">
           <h2>NextJs Documentation &rarr;</h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
-        <a href="https://tailwindcss.com/docs/installation" className={styles.card}>
+        <a href="https://tailwindcss.com/docs/installation" className="card">
           <h2>TailwindCSS Documentation &rarr;</h2>
           <p>Find in-depth information about TailwindCSS features and plugins.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className={styles.card}>
+        <a href="https://nextjs.org/learn" className="card">
           <h2>Learn &rarr;</h2>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
-        <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
+        <a href="https://github.com/vercel/next.js/tree/canary/examples" className="card">
           <h2>Examples &rarr;</h2>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}>
+          className="card">
           <h2>Deploy &rarr;</h2>
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
